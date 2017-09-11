b67e24912cd600c3205339c0741f5a9d51adecf4
EUGinsane <anhhoangit@icloud.com>
2017-09-10 19:47:48 -0700

    update The Journey Begins task 1

---
+++ Arcade/Intro/TheJourneyBegins/add.java
@@ -0,0 +1,27 @@
+/*Write a function that returns the sum of two numbers.
+
+Example
+
+For param1 = 1 and param2 = 2, the output should be
+add(param1, param2) = 3.
+
+Input/Output
+
+[time limit] 3000ms (java)
+[input] integer param1
+
+Guaranteed constraints:
+-100 = param1 = 1000.
+
+[input] integer param2
+
+Guaranteed constraints:
+-100 = param2 = 1000.
+
+[output] integer
+
+The sum of the two inputs. */
+
+int add(int param1, int param2) {
+    return (param1 + param2);
+}
